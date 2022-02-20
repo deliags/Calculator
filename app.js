0545d60fb92a11885c4e2d9407f9487176fd2a9f
deliags <deliagsmith@outlook.com>
2022-02-20 14:07:19 +0000

    added math operations

--- app.js
+++ app.js
@@ -3,9 +3,28 @@ const displayText = document.querySelector(".display-text");
 const displayResult = document.querySelector(".display-result");
 
 const displayValue = (keyPress) => {
-  displayText.textContent += keyPress.textContent;
+  const currentValue = keyPress.textContent;
+  displayText.textContent += currentValue;
 };
 
 const keyPress = buttons.forEach(button => {
   button.addEventListener("click", () => { displayValue(button); });
 });
+
+
+//Operations
+function add(a, b) {
+  return a + b;
+};
+
+function substract(a, b) {
+  return a - b;
+};
+
+function multiply(a, b) {
+  return a * b;
+};
+
+function divide(a, b) {
+  return a / b;
+};
