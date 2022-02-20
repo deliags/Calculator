06f4564d75387a411493058532514cec84d6ff3b
deliags <deliagsmith@outlook.com>
2022-02-20 11:30:48 +0000

    Add keyPress and displayValue behaviour
    The keyPress function is an EventListener for any button click.
    The displayValue function will show the value of the button pressed
    in the display area.

---
+++ app.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll(".button");
+const displayText = document.querySelector(".display-text");
+const displayResult = document.querySelector(".display-result");
+
+const displayValue = (keyPress) => {
+  displayText.textContent = keyPress.textContent;
+};
+
+const keyPress = buttons.forEach(button => {
+  button.addEventListener("click", () => { displayValue(button); });
+});
