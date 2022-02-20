dcb10f1506761619ff2531677b221f1a5c551dd0
deliags <deliagsmith@outlook.com>
2022-02-20 15:48:55 +0000

    add check decimal point exists already

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const buttons = document.querySelectorAll(".button");
-const displayText = document.querySelector(".display-text");
-const displayResult = document.querySelector(".display-result");
+const numberBtn = document.querySelectorAll(".number");
+const operatorBtn = document.querySelectorAll(".operator");
+
+const previousOperand = document.querySelector(".previous-operand");
+const currentOperand = document.querySelector(".current-operand");
+
+const deleteBtn = document.querySelector(".delete");
+const clearBtn = document.querySelector(".clear");
+const equalsBtn = document.querySelector(".equals");
 
 const displayValue = (keyPress) => {
   const currentValue = keyPress.textContent;
-  displayText.textContent += currentValue;
+  if (currentValue === '.' && previousOperand.textContent.includes('.')) return;
+  return (previousOperand.textContent += currentValue);
 };
 
-const keyPress = buttons.forEach(button => {
+numberBtn.forEach(button => {
   button.addEventListener("click", () => { displayValue(button); });
 });
 
@@ -17,7 +25,7 @@ function add(a, b) {
   return a + b;
 };
 
-function substract(a, b) {
+function subtract(a, b) {
   return a - b;
 };
 
@@ -28,3 +36,28 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 };
+
+
+const operate = (firstNum, secNum, operator) => {
+  switch (operator) {
+    case 'plus':
+      return add(firstNum, secNum);
+    case 'minus':
+      return subtract(firstNum, secNum);
+    case 'multiply':
+      return multiply(firstNum, secNum);
+    case 'divide':
+      if(secNum === 0 || secNum === '0') return 'Stop it!'
+      else return divide(firstNum, secNum);
+    default:
+      break;
+  }
+}
+
+const clear = (currentOperand, previousOperand, operation) => {
+  operation = undefined;
+  currentOperand = '';
+  previousOperand = '';
+};
+
+
