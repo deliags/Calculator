cf9d606d0cf4f71d43d413148db82ee243c6a505
deliags <deliagsmith@outlook.com>
2022-02-23 19:19:40 +0000

    initial commit

--- app.js
+++ app.js
@@ -1,63 +1,101 @@
 const buttons = document.querySelectorAll(".button");
 const numberBtn = document.querySelectorAll(".number");
-const operatorBtn = document.querySelectorAll(".operator");
+const operatorBtn = document.querySelectorAll('[data-type="operator"]');
 
-const previousOperand = document.querySelector(".previous-operand");
-const currentOperand = document.querySelector(".current-operand");
+let currentOperand = document.querySelector(".current-operand");
 
 const deleteBtn = document.querySelector(".delete");
 const clearBtn = document.querySelector(".clear");
-const equalsBtn = document.querySelector(".equals");
+const equalsBtn = document.querySelector('[data-type="equals"]');
+
+let firstOperand = '';
+let chosenOperator = '';
 
 const displayValue = (keyPress) => {
   const currentValue = keyPress.textContent;
-  if (currentValue === '.' && previousOperand.textContent.includes('.')) return;
-  return (previousOperand.textContent += currentValue);
+  if (currentValue === '.' && currentOperand.textContent.includes('.')) return;
+
+  firstOperand = (currentOperand.textContent += currentValue);
+  
+  return firstOperand;
 };
 
 numberBtn.forEach(button => {
-  button.addEventListener("click", () => { displayValue(button); });
+  button.addEventListener("click", () => {
+    displayValue(button);
+  });
+});
+
+operatorBtn.forEach(button => {
+  button.addEventListener("click", () => {
+    chosenOperator = button.textContent;
+    displayValue(button);
+    return chosenOperator;
+  });
+});
+
+
+equalsBtn.addEventListener("click", () => {
+  operate(firstOperand, chosenOperator, 0);
+  
 });
 
 
 //Operations
-function add(a, b) {
+function add (a, b) {
+  let operation = (a + b);
+  currentOperand.textContent = operation;
   return a + b;
 };
 
 function subtract(a, b) {
+  currentOperand.textContent = (a - b)
   return a - b;
 };
 
 function multiply(a, b) {
+  currentOperand.textContent = (a * b)
   return a * b;
 };
 
 function divide(a, b) {
+  currentOperand.textContent = (a / b)
   return a / b;
 };
 
 
-const operate = (firstNum, secNum, operator) => {
+const operate = (firstNum, operator, secNum) => {
+  const prev = parseFloat(firstNum);
+  const current = parseFloat(secNum);
+
+  if (isNaN(prev) || isNaN(current)) return;
+
   switch (operator) {
-    case 'plus':
-      return add(firstNum, secNum);
-    case 'minus':
-      return subtract(firstNum, secNum);
-    case 'multiply':
-      return multiply(firstNum, secNum);
-    case 'divide':
-      if(secNum === 0 || secNum === '0') return 'Stop it!'
-      else return divide(firstNum, secNum);
+    case '+':
+      return add(prev, current);
+    case '-':
+      return subtract(prev, current);
+    case '×':
+      return multiply(prev, current);
+    case '÷':
+      if (current === 0 || current === '0') {
+        currentOperand.textContent = 'Stop it!';
+      } else {
+        return divide(prev, current);
+      };
     default:
       break;
   }
 }
 
-const clear = (currentOperand, previousOperand, operation) => {
-  operation = undefined;
-  currentOperand = '';
-  previousOperand = '';
+const clear = () => {
+  currentOperand.textContent  = '';
+  previousOperand.textContent  = '';
+}
+
+function resetDisplay() {
+  currentOperand.textContent = '';
 };
 
-
+clearBtn.addEventListener('click', clear);
+deleteBtn.addEventListener('click', resetDisplay());
